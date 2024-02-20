addd39c2d7170863795def5c40e4b1fbe17b35d8
ishasingh3 <138361249+ishasingh3@users.noreply.github.com>
2024-02-20 22:48:22 +0530

    Add files via upload

---
+++ 03_variable.py
@@ -0,0 +1,5 @@
+character_name = "John"
+character_age = 35
+print("There was once a man named",character_name+",")
+# plus printed without space and , added an extra space
+print("He is ", character_age, "years old.")
---
+++ 04_string.py
@@ -0,0 +1,18 @@
+print("Giraffe\"Academy")
+print("Giraffe\nAcademy")
+print("Giraffe\Academy")
+phrase = "Giraffe Academy"
+print(phrase + " is cool.")
+print(phrase, "is cool.")
+
+# functions
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0]) # string gets indexed from 0
+print(phrase.index("a"))
+print(phrase.index("demy"))
+print(phrase.replace("Giraffe", "Elephant"))
+
